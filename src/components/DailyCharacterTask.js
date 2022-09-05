9a9f8408da8238ed327ea09dd196e7db2543c50f
Thomas Tahk <tomtahk27@gmail.com>
2022-09-05 00:31:41 -0700

    tiny baby step. kinda got a component to render. no idea about image.

---
+++ src/components/DailyCharacterTask.js
@@ -0,0 +1,16 @@
+
+const DailyCharacterTask = () => {
+  return (
+    <>
+      <div>
+        <img src="../img/yoinked_images/chaosdungeon_icon.png" alt="chaos dungeon icon"></img>
+        <label for="chaosdungeon">
+          Chaos Dungeon
+        </label>
+        <input type="checkbox" id="chaosdungeon" name="chaosdungeon"></input>
+      </div>
+    </>
+  );
+}
+
+export default DailyCharacterTask;
