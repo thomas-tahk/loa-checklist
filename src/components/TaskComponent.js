8c7ccbba583e03d156e8bae0966c62caa5f10fb6
Thomas Tahk <tomtahk27@gmail.com>
2022-09-08 20:01:35 -0700

    copied over the show/hide toggle feature from minimum.

--- src/components/TaskComponent.js
+++ src/components/TaskComponent.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const TaskComponent = ({task}) => {
+  const [showStatus, setShowStatus] = useState(true);
+  const buttonText = showStatus ? 'hide':'show';
 
   return (
     <div>
-      <label>
-        <img src = {task.icon} alt = {task.name}/>
-        {task.name}
-        {[...Array(task.numTimes)].map((e, i) => <input key={i} type="checkbox"/>)}
-      </label>
+      {
+        showStatus ?
+        <label>
+          <img src = {task.icon} alt = {task.name}/>
+          {task.name}
+          {[...Array(task.numTimes)].map((e, i) => <input key={i} type="checkbox"/>)}
+        </label>
+        :null
+      }
+
+
+      <button onClick={()=> {
+          setShowStatus(!showStatus);
+        }
+        }>
+          {buttonText}
+        </button>
     </div>
   );
 }
