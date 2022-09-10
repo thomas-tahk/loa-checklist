d58cd158cd917004b8cc5bbe38b876c06c1f20ce
Thomas Tahk <tomtahk27@gmail.com>
2022-09-09 21:40:08 -0700

    Tasks updated to be relatively comprehensive.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -54,6 +54,106 @@ const Tasks = [
       icon: "../img/yoinked_images/anguishedisle_icon.png",
       numTimes: 1
     },
+    {
+      name: "Challenge Guardian Raids",
+      icon: "../img/yoinked_images/guardian_challenge_icon.png",
+      numTimes: 3
+    },
+    {
+      name: "Challenge Guardian Raids",
+      icon: "../img/yoinked_images/guardian_challenge_icon.png",
+      numTimes: 3
+    },
+    {
+      name: "Challenge Abyss Raids",
+      icon: "../img/yoinked_images/challenge_abyss_icon.png",
+      numTimes: 2
+    },
+    {
+      name: "Guild Boss/Siege",
+      icon: "../img/yoinked_images/guildactivities_icon.png",
+      numTimes: 2
+    },
+    {
+      name: "PVP Token Exchange",
+      icon: "../img/yoinked_images/pvpexchange_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Ghost Ship",
+      icon: "../img/yoinked_images/ghostship_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Siege Event Island",
+      icon: "../img/yoinked_images/pvpisland_icon.png",
+      numTimes: 2
+    },
+    {
+      name: "Chaos Line",
+      icon: "../img/yoinked_images/guildactivities_icon.png",
+      numTimes: 2
+    },
+    {
+      name: "Event Exchange 1 (Maharaka Leaf)",
+      icon: "../img/yoinked_images/gold_or_event_exchange.png",
+      numTimes: 1
+    },
+    {
+      name: "Event Exchange 2 (Winter Illusion)",
+      icon: "../img/yoinked_images/eventguardian_exchange_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Abyss Dungeon",
+      icon: "../img/yoinked_images/abyss_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Abyss Raid (Argos)",
+      icon: "../img/yoinked_images/abyssraid_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Valtan Legion Raid",
+      icon: "../img/yoinked_images/legionraid_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Vykas Legion Raid",
+      icon: "../img/yoinked_images/legionraid_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Valtan Legion Raid",
+      icon: "../img/yoinked_images/legionraid_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Una Weeklies",
+      icon: "../img/yoinked_images/unaweekly_icon.png",
+      numTimes: 3
+    },
+    {
+      name: "Guild Weekly Tasks",
+      icon: "../img/yoinked_images/guildweekly_icon.png",
+      numTimes: 3
+    },
+    {
+      name: "Merchant Ship Exchange",
+      icon: "../img/yoinked_images/piratecoin_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Sylmael Bloodstone Exchange",
+      icon: "../img/yoinked_images/sylmaelbloodstone_icon.png",
+      numTimes: 1
+    },
+    {
+      name: "Chaos Dungeon Shard Exchange",
+      icon: "../img/yoinked_images/chaosdungeon_icon.png",
+      numTimes: 1
+    },
 ];
 
 export default Tasks;
